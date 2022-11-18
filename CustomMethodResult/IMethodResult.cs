ea067d2802522e15623da0ba110b656c04e25622
Vladimir Taranchuk <VladimirTaranchuk@gmail.com>
2022-11-18 23:52:10 +0300

    Method Result вынесен в отдельную библиотеку Custom Method Result

---
+++ CustomMethodResult/IMethodResult.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomMethodResult
+{
+    public interface IMethodResult<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
